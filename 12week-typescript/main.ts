9c49bc1eca480b462176499344e9ed1a4caf5627
jjunhyuki <yjs99004@naver.com>
2024-02-07 18:42:31 +0900

    typescript-ing

--- 12week-typescript/main.ts
+++ 12week-typescript/main.ts
@@ -200,3 +200,278 @@ console.log(stock, cart);
 const result2 = addToCart('c002', 2);
 console.log(`결과1: ${result2}`);
 console.log(stock, cart);
+
+//3.Enum 01. Enum
+
+//enum은 왠만해선 값을 정해놓고 쓰는게 좋다.
+// enum Size {S, M, L, XL,}
+enum Size {
+  S = 'S',
+  M = 'M',
+  L = 'L',
+  XL = 'XL',
+}
+
+let product01: {
+  id: string;
+  name: string;
+  price: number;
+  membersOnly?: boolean;
+  sizes: Size[];
+} = {
+  id: 'c001',
+  name: '코드잇 블랙 후디',
+  price: 129000,
+  sizes: [Size.M, Size.L],
+};
+
+function findProcudt(size?: Size) {
+  if (!size) {
+    console.log('전체 사이즈로 검색');
+    return;
+  }
+  console.log('특정 사이즈로 검색');
+}
+
+// enum은 0부터
+console.log(Size.S); //값을 안정해줬을때에는 0 , 값을정해줬을때에는 해당하는 값이 나온다. => S
+console.log(Size.M); //1 => M
+console.log(Size.L); //2 => L
+console.log(Size.XL); //3 => XL
+console.log();
+
+/*-------------------------------------------------------------------------------------*/
+
+//4.interface 01.interface
+
+// 같은 타입을 여러곳에서 사용하고 정의하는것이 비효율적이다 그럴때 쓰는데 interface
+
+enum Size1 {
+  S = 'S',
+  M = 'M',
+  L = 'L',
+  XL = 'XL',
+}
+
+// let interface1: {
+//   id: string;
+//   name: string;
+//   price: number;
+//   membersOnly?: boolean;
+//   sizes: Size[];
+// } = {
+//   id: 'c001',
+//   name: '코드잇 블랙 후디',
+//   price: 129000,
+//   sizes: [Size.M, Size.L],
+// };
+
+// let interface2: {
+//   id: string;
+//   name: string;
+//   price: number;
+//   membersOnly?: boolean;
+//   sizes: Size[];
+// } = {
+//   id: 'c001',
+//   name: '코드잇 블랙 후디',
+//   price: 129000,
+//   sizes: [Size.M, Size.L],
+// };
+
+interface InterfaceProduct {
+  id: string;
+  name: string;
+  price: number;
+  membersOnly?: boolean;
+}
+
+//interface  에서는 상속이 가능하다는 장점이있다.
+interface ClothingProduct extends InterfaceProduct {
+  sizes: Size1[];
+}
+
+// const interface1: InterfaceProduct = {
+const interface1: ClothingProduct = {
+  id: 'c001',
+  name: '코드잇 블랙 후디',
+  price: 129000,
+  membersOnly: true,
+  sizes: [Size1.M, Size1.L],
+};
+
+const interface2: InterfaceProduct = {
+  id: 'c001',
+  name: '코드잇 블랙 후디',
+  price: 129000,
+  membersOnly: true,
+};
+
+//interface는 함수도 정의할수있다.
+interface printProductionFunction {
+  (product: InterfaceProduct): void;
+}
+
+// function printProduction(product: InterfaceProduct) {
+const printProduction: printProductionFunction = InterfaceProduct => {
+  console.log(
+    `${InterfaceProduct.name}의 가격은 ${InterfaceProduct.price}원 입니다.`,
+  );
+};
+
+printProduction(interface1);
+printProduction(interface2);
+
+// 05 그밖의 타입들 01. 리터럴
+
+let productName1 = '코드잇 블랙 후드'; //productName1 은 string이 타입
+const productName2 = '코드잇 텀블러'; //productNmae2 는 '코드잇 텀블러'가 타입
+//ㄴ> let은 값이 바뀔수있고 const 값이 안바뀌어서 '코드잇 텀블러'를 타입으로가진다 , 이런걸 리터럴 타입이라 한다.
+
+let small = 95;
+const large = 100; // 100을 타입으로 가지게된다.
+
+function printSize(size: number) {
+  console.log(`${size}: 사이즈 입니다.`);
+}
+
+// function printSize(sizde: 100) {
+// ㄴ> 만약 값을 이렇게 받는다면
+// printSize(small); 은 에러가 뜨게된다.
+
+printSize(small);
+printSize(large);
+
+//즉 리터럴 타입은 값을 곧 타입으로 받는다 라는것
+
+//02. 타입별칭
+type Cart2 = string[];
+
+type CartResultCallback = (result: boolean) => void;
+
+//이렇게 객체형태 타입을 정할수 있지만
+// type Product = {
+//   id: string;
+//   name: string;
+// };
+
+//왠만해서는 객체형태 타입은 인터페이스를 사용하는건 권장 (아래코드와 같이)
+interface product02 {
+  id: string;
+  name: string;
+}
+
+const Cart2: Cart2 = ['c001', 'c002', 'c003'];
+
+interface User {
+  username: string;
+  email: string;
+  cart: Cart2;
+}
+
+const user: User = {
+  username: 'codeit',
+  email: 'typescript@codeit.kr',
+  cart,
+};
+
+//04 Union 타입
+
+enum ClothingSize {
+  S = 'S',
+  M = 'M',
+  L = 'L',
+  XL = 'XL',
+}
+
+// type ClothingSize = 'S' | 'M' | 'L' | 'XL'; // 이렇게 리터럴로도 사용도 가능하지만 어떨때 사용하는게 좋은지는 다음 챕터에서 확인
+
+interface ProductUnion {
+  id: string;
+  name: string;
+  price: number;
+  membersOnly?: boolean;
+}
+
+interface ClothingProductUnion extends ProductUnion {
+  sizes: ClothingSize[];
+  color: string;
+}
+
+//유니온 타입은 리터럴 타입이랑 같이쓰면 효율이 급상승한다.
+type ShoeSize =
+  | 220
+  | 225
+  | 230
+  | 235
+  | 240
+  | 245
+  | 250
+  | 255
+  | 260
+  | 265
+  | 270
+  | 275
+  | 280;
+interface shoeProductUnion extends ProductUnion {
+  sizes: number[];
+  // sizes: ShoeSize;
+  handmade: boolean;
+}
+
+function printSizes(product: ClothingProductUnion | shoeProductUnion) {
+  //받는 값을 ClothingProduct이거나 shoeProduct 이라는뜻 이렇게 쓰는게 유니온 타입
+  const availableSizes = product.sizes.join(', ');
+  console.log(`구매 가능한 사이즈는 다음과 같습니다: ${availableSizes}`);
+
+  // 특정한 타입에 대해서 뭔가 하고싶다면  if, in을 사용해보자
+  if ('color' in product) {
+    console.log(`색상: ${product.color}`);
+  }
+
+  if ('handmade' in product) {
+    console.log(
+      product.handmade
+        ? '이 상품은 장인이 직접 만듭니다.'
+        : '이 상품은 공장에서 만들어 졌습니다.',
+    );
+  }
+}
+
+// 06. intersection 타입 => 타입을 합치는 법
+
+interface Id {
+  id: string;
+}
+
+interface Timestamp {
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+//이게 인터섹션
+type ProdcutIntersection = Id & {
+  // id: string;
+  name: string;
+  price: number;
+  membersOnly?: boolean;
+};
+
+type UserIntersection = Id &
+  Timestamp & {
+    // id: string;
+    username: string;
+    email: string;
+    // createdAt: Date;
+    // updatedAt: Date;
+  };
+
+type Review = Id &
+  Timestamp & {
+    // id: string;
+    productId: string;
+    userId: string;
+    content: string;
+    // createdAt: Date;
+    // updatedAt: Date;
+  };
