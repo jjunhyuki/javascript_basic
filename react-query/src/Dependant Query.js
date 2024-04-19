930bf65ac355e4c488efe139ab45b564bc1bf92b
jjunhyuki <yjs99004@naver.com>
2024-04-19 11:27:22 +0900

    Dependant Query

---
+++ react-query/src/Dependant Query.js
@@ -0,0 +1,5 @@
+//만약 어떤 두 쿼리가 의존관계가 있어 어떤 특정 순서대로 실ㅇ 되어야 하는 겨웅에는 어떻게 해야 할까?
+//그럴때 사용하는게 Dependant Query이다.
+// ㄴ> Dependant Query란  이전에 수행을 마친 쿼리에게 의존하는 쿼리를 뜻한다.
+//  ㄴ>enabled 값으로 이전 쿼리 결과를 넣어주면, 그 쿼리 결과가 도출될 때까지 해당 쿼리가 실행되지 않는 식이다.
+// 참고 : https://velog.io/@js43o/React-React-Query-%EA%B8%B0%EB%B3%B8-%EC%A0%95%EB%A6%AC
